dfec7677df2d96b18f4f4965dfb2cc14721dbdc4
bharatsinghtanwar <bharat.tanwar.work@gmail.com>
2025-08-25 22:39:24 +0530

    initial commit

---
+++ backend/backend/src/main/java/com/shopsmart/backend/ShopSmartBackendApplication.java
@@ -0,0 +1,12 @@
+package com.shopsmart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSmartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopSmartBackendApplication.class, args);
+    }
+}
---
+++ backend/backend/src/test/java/com/shopsmart/backend/ShopSmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsmart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
